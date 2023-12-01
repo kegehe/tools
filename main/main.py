cb3db376411f911c93fce82dcafb7d1c66585bd1
tmk <tmk@thinkerx.com>
2023-12-01 11:49:57 +0800

    feat: jenkins构建测试

--- main/main.py
+++ main/main.py
@@ -9,5 +9,10 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/hello")
+async def func1():
+    return {'status': 1, 'msg': '请求成功', 'data': 'jenkins'}
+
+
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
